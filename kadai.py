6330acaf3c8dd7c4e00cae3169e3dd4ab457aa1e
Amit Prasad <17amitprasad@gmail.com>
2020-01-08 10:13:52 -0500

    Added proper quit behaviour (with 'q'), pretty print dates, and better formatting

--- kadai.py
+++ kadai.py
@@ -1,10 +1,65 @@
 import time
 from datetime import datetime
+import math
+import sys
 
 import dateparser
-from blessings import Terminal
+from blessed import Terminal
+title = '''    ,,,,,,,,,,
 
+   ▐██████████▌  ▐██████████████████▌       ██████████████  █████████████████▌
+    ``````````   ▐███▀""╙▀███M""╙███▌       ███▌`````▐████  ``````▐███▀```````
+ ▐█████████████H ▐███▌,,,║██▌,,,╓███▌       ████▓▓▓▓▓█████   ▄▄▄▄▄████▄▄▄▄▄▄▄
+ ╙▀▀▀▀▀▀▀▀▀▀▀▀▀╩ ▐██████████████████▌       ███▀"""""╙████   ║███▀▀▀▀▀▀▀▀███▌
+   ,,,,,,,,,,,,  ▐███▌```║██▌"``"███▌       ████▄▄▄▄▄▄████   ║██▌,,,,,,,▄███▌
+   ▐██████████▌  ▐███▄╓╓╓▄███▄╓╓▄███▌       ▀▀▀▀▀▀▀▀▀▀▀▀▀▀   ║██████████████▌
+    ``````````   ▐██████████████████▌                        ║██▌       ▐███▌
+   ╔▄▄▄▄▄▄▄▄▄▄▄   ```````████M``````      ▓████████████████M ║██████████████▌
+   ║██████████▌ ╓▄▄▄▄▄▄▄▄████▓▄▄▄▄▄▄▄▄    ╙╜╜╜╜╜╜▀███▀╜╜╜╜╜^ ║██▌```````▐███▌
+                ▐█████████████████████      ▄▄▄  ▐███        ║███▌▄▄▄▄▄▄▓███▌
+   ▐██████████▌  `````╠████████▌           ▐███  ▐████████▌  ╝▀▀▀▀▀▀▀▀▀▀▀▀▀▀╧
+   ▐███╨╙╙╙███▌     ,▄███████████▄         ║███▄ ▐███▀╜╜╜╜╜   ╓▄██▄⌐  τ▓██▄,
+   ▐██▌    ║██▌   ╓▄████╙████M╙████▄       ║████▄▄██▌     ,╓▄████▀      ╙████▄
+   ▐██▌w╓╓╓███▌╓▄█████╜  ║███  `▀████▄,   ╓███▀██████,     "▀█▀`          ╙▀▀"
+   ▐██████████▌`▀███╨    ║███    `▀██╩   ╓███▀  "▀████████▓▓▄▄▄▄▄▄▄▄▄▄▄▄▄▓▓▄▓▓Γ
+   ║██▌```````   ``      ║███      `      ╙█▀       `╙╙▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+  '''
+def pretty_date(time):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    now = datetime.now()
+    diff = time-now
+    second_diff = diff.seconds
+    day_diff = diff.days
 
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "now"
+        if second_diff < 60:
+            return "in " + str(second_diff) + " seconds"
+        if second_diff < 120:
+            return "in a minute"
+        if second_diff < 3600:
+            return "in " + str(math.floor(second_diff / 60)) + " minutes"
+        if second_diff < 7200:
+            return "in an hour"
+        if second_diff < 86400:
+            return "in " + str(math.floor(second_diff / 3600)) + " hours"
+    if day_diff == 1:
+        return "Tomorrow"
+    if day_diff < 7:
+        return "in " + str(math.floor(day_diff)) + " days"
+    if day_diff < 31:
+        return "in " + str(math.floor(day_diff / 7)) + " weeks"
+    if day_diff < 365:
+        return "in " + str(math.floor(day_diff / 30)) + " months"
+    return "in " + str(math.floor(day_diff / 365)) + " years"
 class Task:
     def __init__(self, value, date):
         self.value = value
@@ -14,7 +69,7 @@ class Task:
         return self.date - datetime.now()
 
     def __str__(self):
-        return self.value + ' ' + str(self.time_till_expire())
+        return self.value + ' ' + pretty_date(self.date)
 
 
 class TaskManager:
@@ -25,9 +80,10 @@ class TaskManager:
 
     def update_tasks(self):
         print(self.term.move(0, 0), end='')
-        if self.selected >= len(self.tasks):
+        if self.selected >= len(self.tasks) or self.selected < 0:
             self.selected = self.selected % len(self.tasks)
         for i, t in enumerate(self.tasks):
+            print(self.term.color(5)(str(i+1)+'. '),end='\t')
             if i == self.selected:
                 print(self.term.black_on_white(str(t)),
                       end=self.term.move_down)
@@ -37,14 +93,21 @@ class TaskManager:
 
 if __name__ == "__main__":
     term = Terminal()
+    tm = TaskManager(term)
     with term.fullscreen():
-        tm = TaskManager(term)
-        for i in range(10):
+        for i in range(0,10):
             tm.tasks.append(
-                Task("Test", dateparser.parse("in {0} hours".format(i))))
-        while True:
-            #   print(term.clear, end='')
+                Task("Test", dateparser.parse("in {0} minutes".format(i))))
+    while True:
+        with term.fullscreen(),term.cbreak():
             tm.update_tasks()
-            tm.selected += 1
-            time.sleep(.1)
-            #tm.tasks.append(Task("Test", dateparser.parse(input())))
+            inp = term.inkey(timeout=1)
+            raw = repr(inp).replace("'","")
+            if not inp:
+                pass
+            elif raw == u'KEY_UP':
+                tm.selected -= 1
+            elif raw == u'KEY_DOWN':
+                tm.selected += 1
+            elif raw in (u'q', u'Q'):
+                sys.exit()
